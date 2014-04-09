3239571c6b2d460f5819027f35b3f7e2d890380e
leahsteinberg <leahsteinberg1@gmail.com>
2014-04-09 19:49:19 -0400

    added css stuff

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ from flask import request, redirect
 import random, requests, dinologic
 
 app = Flask(__name__)
+#app._static_folder = "../bootstrap/css/bootstrap.min.css"
 app.config['DEBUG'] = True
 
 
@@ -16,16 +17,27 @@ def redir_Url(key):
     if url is not '':
       return redirect('http://' + url, code = 302)
     else:
-        return 'Error: id does not exsist'
+        return 'Error: id does not exist'
+
+@app.route("/favicon.ico")
+def favicon():
+    return app.send_static_file("favicon.ico")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    print '1111'
     if request.method == 'POST':
+        print "222222"
         url = request.form['linkbox']
+        print "3333333 "
         
         tiny_url = dinologic.make_tiny(url, request.host)
         return render_template('redir.html', long_url = 'http://'+url, tiny_url = tiny_url)
     else:
+        print "444"
+        print request
+        print "555"
         return render_template('index.html')
 
 if __name__ == '__main__':
